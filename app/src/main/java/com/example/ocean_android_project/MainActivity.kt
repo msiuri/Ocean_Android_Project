859e2fb928b22fa67ca12fec8baf7f6da21a6cd4
Iuri Moura <msiuri@outlook.com>
2022-09-13 11:45:20 -0300

    Add função para o botão "Enviar"

--- app/src/main/java/com/example/ocean_android_project/MainActivity.kt
+++ app/src/main/java/com/example/ocean_android_project/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.ocean_android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Detectar clique
+
+        //Pegamos o item na tela através do ID
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        //Adciona um listener para o btEnviar
+        btEnviar.setOnClickListener {
+            //Executa ao clicar no botão
+
+            tvResultado.text = "Iuri Moura"
+        }
+
+
+
     }
 }
