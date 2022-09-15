13f39730076ff6f1b2dae09389e53387afaf6ab4
Iuri Moura <msiuri@outlook.com>
2022-09-15 09:54:54 -0300

    Criação de tela Resultado

--- app/src/main/java/com/example/ocean_android_project/MainActivity.kt
+++ app/src/main/java/com/example/ocean_android_project/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.ocean_android_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +18,28 @@ class MainActivity : AppCompatActivity() {
         //Pegamos o item na tela através do ID
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+
 
         //Adciona um listener para o btEnviar
         btEnviar.setOnClickListener {
             //Executa ao clicar no botão
+            if (etNome.text.isNotEmpty() ) {
+                tvResultado.text = etNome.text
+            }
+            else {
+                val msgError = "Digite algo!"
+                val duration = Toast.LENGTH_SHORT
 
-            tvResultado.text = "Iuri Moura"
+                val toast = Toast.makeText(applicationContext, msgError, duration)
+
+                toast.show()
+
+            }
         }
+
+
 
 
 
